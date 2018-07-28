c0ea8e74e565a0c8fcfbc18ea5f7bf82d8f20fba
Jiahao Huang <1098278937@qq.com>
2018-07-28 13:26:41 +0800

    JH_7777777

--- ExperienceCentr/AppDelegate.m
+++ ExperienceCentr/AppDelegate.m
@@ -17,6 +17,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor clearColor]} forState:UIControlStateNormal];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
